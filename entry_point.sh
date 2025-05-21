16416ebe941da2e70858023cf6d35fad9eb3c39f
Yuval Turgeman <yturgema@redhat.com>
2025-05-21 10:47:28 -0400

    Use pg and kafka configuration from env
    Signed-off-by: Yuval Turgeman <yturgema@redhat.com>

--- entry_point.sh
+++ entry_point.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+echo "entry point starting"
+yq eval ".project = \"$FEAST_PROJECT_NAME\"" -i /app/feature_store.yaml
+yq eval ".registry.path = \"$FEAST_REGISTRY_URL\"" -i /app/feature_store.yaml
+yq eval ".online_store.host = \"$DB_HOST\"" -i /app/feature_store.yaml
+yq eval ".online_store.port = \"$DB_PORT\"" -i /app/feature_store.yaml
+yq eval ".online_store.user = \"$DB_USER\"" -i /app/feature_store.yaml
 yq eval ".online_store.password = \"$DB_PASSWORD\"" -i /app/feature_store.yaml
+yq eval ".online_store.database = \"$DB_NAME\"" -i /app/feature_store.yaml
 cat /app/feature_store.yaml
+echo "entry point ending"
 python main.py
